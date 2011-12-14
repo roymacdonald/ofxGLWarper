9be64fa8ec94543835da754152841aa07403fef1
eloimaduell <eloi@telenoika.net>
2011-12-14 03:43:51 +0100

    changes

--- src/ofxGLWarper.h
+++ src/ofxGLWarper.h
@@ -4,14 +4,22 @@
 #define OF_ADDON_USING_OFOPENCV
 
 #include "ofMain.h"
-
+// ofxGLWarper by Roy Macdonald
+// ... / ... 
+//
+// -- little changes made by Eloi Maduell - 12/2011
+// -- thanks to Arturo and hv_francesco 
+// added resolution of the quad in setup()
+// changed draw() to begin() end() model
+// added fromScreenToWarpCoord() and fromWarpToScreenCoord()
 
 class ofxGLWarper{
 	
 public:
 		
-	void setup();
-	void draw();
+	void setup(int _resX, int _resY); //changed to have resolution as parameter for the quad
+	void begin();	//changed name from draw to begin
+	void end();		//added to make it easier to use, similar to ofFbo (begin,end)
 		
 	void mouseDragged(ofMouseEventArgs &args);
 	void mousePressed(ofMouseEventArgs &args);
@@ -23,7 +31,12 @@ public:
 	void deactivate();
 	bool isActive();
 	
+	ofVec4f		fromScreenToWarpCoord(float x,float y,float z);
+	ofVec4f		fromWarpToScreenCoord(float x,float y,float z);
+
 private:
+	int  width; //width of the quad to work with
+	int	 height; // height of the quad to work with
 	bool active;
 	ofPoint corners[4];
 	int whichCorner;
