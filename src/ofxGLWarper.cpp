67dd97cb1ff34282fb84440d4050c0afb5f903b5
Roy Macdonald <macdonald.roy@gmail.com>
2016-09-20 09:03:05 -0300

    Fixed openCV includes for OF0.9.3_release version

--- src/ofxGLWarper.cpp
+++ src/ofxGLWarper.cpp
@@ -1,7 +1,7 @@
 #include "ofxGLWarper.h"
 #include "stdio.h"
-#include "opencv2/calib3d/calib3d_c.h"
-
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/core/core_c.h"
 
 //--------------------------------------------------------------
 void ofxGLWarper::setup(){
