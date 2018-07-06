9c5ddb36f39e786254ce1983658a0830c2b0341c
unknown <cyril.communal@gmail.com>
2018-07-06 04:54:33 +0200

    new processMatrices() without openCV

--- src/ofxGLWarper.cpp
+++ src/ofxGLWarper.cpp
@@ -1,6 +1,5 @@
 #include "ofxGLWarper.h"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/core/core_c.h"
+#include "ofxHomography.h"
 
 //--------------------------------------------------------------
 void ofxGLWarper::setup(){
@@ -95,88 +94,28 @@ void ofxGLWarper::processMatrices(){
 	//and 1.0 scale for x y z and w
     myMatrix = ofMatrix4x4(); // default constructor generates identity
 	
-	//we need our points as opencv points
-	//be nice to do this without opencv?
-	CvPoint2D32f cvsrc[4];
-	CvPoint2D32f cvdst[4];	
+    //ofPoints instead of openCV points
+    ofPoint cvsrc[4];
+    ofPoint cvdst[4];
 	
 	//we set the warp coordinates
 	//source coordinates as the dimensions of our window
-	cvsrc[0].x = x;
-	cvsrc[0].y = y;
-	cvsrc[1].x = x+width;
-	cvsrc[1].y = y;
-	cvsrc[2].x = x+width;
-	cvsrc[2].y = y+height;
-	cvsrc[3].x = x;
-	cvsrc[3].y = y+height;			
-	
+    cvsrc[0].set(x,y);
+    cvsrc[1].set(x+width,y);
+    cvsrc[2].set(x+width,y+height);
+    cvsrc[3].set(x,y+height);
+
 	//corners are in 0.0 - 1.0 range
 	//so we scale up so that they are at the window's scale
-	for(int i = 0; i < 4; i++){
-        //cvdst[i].x = corners[i].x  * static_cast<float>(width);
-        //cvdst[i].y = corners[i].y * static_cast<float>(height);
-        cvdst[i].x = corners[i].x;
-		cvdst[i].y = corners[i].y;
+    for(int i = 0; i < 4; i++){
+        cvdst[i].set(corners[i].x,corners[i].y);
 	}
-	
-	//we create a matrix that will store the results
-	//from openCV - this is a 3x3 2D matrix that is
-	//row ordered
-	CvMat * translate = cvCreateMat(3,3,CV_32FC1);
-	
-	//this is the slightly easier - but supposidly less
-	//accurate warping method 
-	//cvWarpPerspectiveQMatrix(cvsrc, cvdst, translate); 
-	
-	
-	//for the more accurate method we need to create
-	//a couple of matrixes that just act as containers
-	//to store our points  - the nice thing with this 
-	//method is you can give it more than four points!
-	
-	CvMat* src_mat = cvCreateMat( 4, 2, CV_32FC1 );
-	CvMat* dst_mat = cvCreateMat( 4, 2, CV_32FC1 );
-	
-	//copy our points into the matrixes
-	cvSetData( src_mat, cvsrc, sizeof(CvPoint2D32f));
-	cvSetData( dst_mat, cvdst, sizeof(CvPoint2D32f));
+
 	
 	//figure out the warping!
-	//warning - older versions of openCV had a bug
-	//in this function.
-	cvFindHomography(src_mat, dst_mat, translate);
 	
-	//get the matrix as a list of floats
-	float *matrix = translate->data.fl;
-	
-	
-	//we need to copy these values
-	//from the 3x3 2D openCV matrix which is row ordered
-	//
-	// ie:   [0][1][2] x
-	//       [3][4][5] y
-	//       [6][7][8] w
-	
-	//to openGL's 4x4 3D column ordered matrix
-	//        x  y  z  w   
-	// ie:   [0][3][ ][6]
-	//       [1][4][ ][7]
-	//		 [ ][ ][ ][ ]
-	//       [2][5][ ][8]
-	//       
+    myMatrix = ofxHomography::findHomography(cvsrc, cvdst);
 
-        myMatrix(0,0) = matrix[0];
-        myMatrix(1,0) = matrix[1];
-        myMatrix(3,0) = matrix[2];
-
-        myMatrix(0,1) = matrix[3];
-        myMatrix(1,1) = matrix[4];
-        myMatrix(3,1) = matrix[5];
-
-        myMatrix(0,3) = matrix[6];
-        myMatrix(1,3) = matrix[7];
-        myMatrix(3,3) = matrix[8];
 }
 //--------------------------------------------------------------
 void ofxGLWarper::draw(){
